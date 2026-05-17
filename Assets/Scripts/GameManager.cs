ef22255de123c35d0ce35c464c8bf0736f69b97d
Nuria Sy <2526_nuria.sy@iticbcn.cat>
2026-05-17 11:57:07 +0200

    Afegida plataforma mòbil

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    private Vector3 startPos = new Vector3(-6.27f, -3.25f, 0f);
+    private Vector3 startPos = new Vector3(-6.27f, -3.334f, 0f);
 
     private void Awake()
     {
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -69,4 +69,20 @@ public class Player : MonoBehaviour
         sr.enabled = true;
         isInvincible = false;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+        {
+            transform.parent = collision.transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+        {
+            transform.parent = null;
+        }
+    }
 }
